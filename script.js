e01962b5ea0f58e347ce0d5fb74eb6974b0dacca
Kejsi Llenga <keisillenga@gmail.com>
2026-08-15 09:05:09 +0200

    script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const colorPicker = document.getElementById("color");
+const sizeSlider = document.getElementById("size");
+const sizeValue = document.getElementById("sizeValue");
+
+const pencilBtn = document.getElementById("pencilBtn");
+const eraserBtn = document.getElementById("eraserBtn");
+const clearBtn = document.getElementById("claerBtn");
+const saveBtn = document.getElementById("saveBtn");
+const undoBtn = document.getElementById("undoBtn");
+const redoBtn = document.getElementById("redoBtn");
